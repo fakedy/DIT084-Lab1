62d5f9bc4270d9940ac34902b221502c49dcba17
fakedy <34602297+fakedy@users.noreply.github.com>
2024-11-04 22:16:15 +0100

    Initial commit

---
+++ tests/SetTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetTest {
+
+    @Test
+    void toArray() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void member() {
+    }
+
+    @Test
+    void intersect() {
+    }
+
+    @Test
+    void distinctClosed() {
+    }
+}
